26cfab330d0f256eb9a7763afadbb2cc7867ec29
Adam Young <adamyoungg97@gmail.com>
2019-05-09 11:37:43 +0100

    Combining Multiple Scans To Create One Room
    Code now loops through the scans, collecting walls until it has all 4.
    
    Co-Authored-By: Thomas Wheatley <t-wheatley@users.noreply.github.com>

--- UI/src/app/components/paper-canvas/paper-canvas.component.ts
+++ UI/src/app/components/paper-canvas/paper-canvas.component.ts
@@ -1,53 +1,29 @@
-import { Measurement } from './../../classes/measurement';
-import { Direction } from './../../classes/wall';
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { PaperScope, Project, Path, Point, view, Matrix, Rectangle, Size } from 'paper';
-import { Coordinate } from 'src/app/classes/coordinate';
-import { Wall } from 'src/app/classes/wall';
-import { Room } from '../../classes/room';
-import { PositionalEntry } from '../../classes/positionalEntry';
-import { JsonObject, JsonProperty, JsonConvert } from 'json2typescript';
-import { Direct } from 'protractor/built/driverProviders';
+import { Direction } from "./../../classes/wall";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { PaperScope, Project, Path, Point, Rectangle, Size } from "paper";
+import { Coordinate } from "src/app/classes/coordinate";
+import { Wall } from "src/app/classes/wall";
+import { Room } from "../../classes/room";
+import { PositionalEntry } from "../../classes/positionalEntry";
+import { JsonConvert } from "json2typescript";
 
 @Component({
-  selector: 'app-paper-canvas',
-  templateUrl: './paper-canvas.component.html',
-  styleUrls: ['./paper-canvas.component.css']
+  selector: "app-paper-canvas",
+  templateUrl: "./paper-canvas.component.html",
+  styleUrls: ["./paper-canvas.component.css"]
 })
 export class PaperCanvasComponent implements OnInit {
-  @ViewChild('canvasElement') canvasElement: ElementRef;
+  @ViewChild("canvasElement") canvasElement: ElementRef;
   scope: PaperScope;
   project: Project;
   file: File;
+  currentRoom: Room;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.scope = new PaperScope();
     this.project = new Project(this.canvasElement.nativeElement);
-
-    /*
-    // Mark center
-    const centerMarker = new Path.Rectangle(
-      new Rectangle(new Point(this.scope.view.center.x - 1, this.scope.view.center.x - 1), new Size(2, 2))
-    );
-    centerMarker.fillColor = 'red';
-    */
-
-    /*
-    // Draw walls
-    const origin = new Coordinate(this.scope.view.center.x, this.scope.view.center.y);
-    const walls = [
-      new Wall(origin, Direction.Forward, [30, 30]),
-      new Wall(origin, Direction.Backward, [20, 20]),
-      new Wall(origin, Direction.Right, [20, 20]),
-      new Wall(origin, Direction.Left, [20, 20]),
-    ];
-
-    const room = new Room(walls);
-    room.draw();
-    room.detectIntersections();
-    */
   }
 
   fileChange(event: EventTarget) {
@@ -57,43 +33,51 @@ export class PaperCanvasComponent implements OnInit {
     this.file = files[0];
 
     const reader = new FileReader();
-    reader.onload = (e) => {
+    reader.onload = e => {
       const contents = (<any>e).target.result;
 
       const jsonConvert: JsonConvert = new JsonConvert();
       const entries = JSON.parse(contents);
 
-      const viewCenter = new Coordinate(this.scope.view.center.x, this.scope.view.center.y);
-      console.log(viewCenter);
+      const viewCenter = new Coordinate(
+        this.scope.view.center.x,
+        this.scope.view.center.y
+      );
+
+      const walls = [] as Wall[];
 
       entries.forEach(entry => {
-        const reading: PositionalEntry = jsonConvert.deserializeObject(entry, PositionalEntry);
+        const reading: PositionalEntry = jsonConvert.deserializeObject(
+          entry,
+          PositionalEntry
+        );
         console.log(reading);
 
-        // tslint:disable-next-line:max-line-length
-        const origin = new Coordinate(viewCenter.x + this.scaleEncoderValue(reading.encoderXPos), viewCenter.y - this.scaleEncoderValue(reading.encoderYPos));
+        const origin = new Coordinate(
+          viewCenter.x + this.scaleEncoderValue(reading.encoderXPos),
+          viewCenter.y - this.scaleEncoderValue(reading.encoderYPos)
+        );
         const originMarker = new Path.Rectangle(
           new Rectangle(new Point(origin.x - 1, origin.x - 1), new Size(2, 2))
         );
-        originMarker.fillColor = 'red';
+        originMarker.fillColor = "red";
 
+        console.log("origin:");
         console.log(origin);
-
-        const walls = [];
 
         reading.measurements.forEach(measurement => {
           let direction;
           switch (measurement.direction) {
-            case 'Forward':
+            case "Forward":
               direction = Direction.Forward;
               break;
-            case 'Back':
+            case "Back":
               direction = Direction.Backward;
               break;
-            case 'Left':
+            case "Left":
               direction = Direction.Left;
               break;
-            case 'Right':
+            case "Right":
               direction = Direction.Right;
               break;
             default:
@@ -101,33 +85,41 @@ export class PaperCanvasComponent implements OnInit {
           }
 
           if (measurement.leftSensor === 0 && measurement.rightSensor === 0) {
-            console.log('No sensor readings!');
             return;
-          } else if (measurement.leftSensor === 0) {
-            console.log('No left sensor reading!');
+          }
+
+          if (measurement.leftSensor === 0) {
+            console.log("No left sensor reading!");
             measurement.leftSensor = measurement.rightSensor;
           } else if (measurement.rightSensor === 0) {
-            console.log('No right sensor reading!');
+            console.log("No right sensor reading!");
             measurement.rightSensor = measurement.leftSensor;
           }
 
-          const wall = new Wall(origin, direction, [measurement.leftSensor, measurement.rightSensor]);
-          console.log(wall);
-          walls.push(wall);
+          if (walls.filter(w => w.direction === direction).length == 0) {
+            const wall = new Wall(origin, direction, [
+              measurement.leftSensor,
+              measurement.rightSensor
+            ]);
+            console.log(wall);
+            walls.push(wall);
+          }
         });
-
-        const room = new Room(walls);
-        room.draw();
-        room.detectIntersections();
-
-
-
       });
+
+      if (walls.length !== 4) {
+        alert("Missing one or more walls.");
+        return;
+      }
+
+      this.currentRoom = new Room(walls);
+      this.currentRoom.draw();
+      this.currentRoom.detectIntersections();
     };
     reader.readAsText(this.file);
   }
 
   scaleEncoderValue(number) {
-    return ((number / 100) * 250);
+    return (number / 100) * 250;
   }
 }
